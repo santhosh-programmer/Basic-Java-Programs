da2dd3e3d125d2dd88cd6074711a26d2f48011ad
santhosh <105176741+santhosh-programmer@users.noreply.github.com>
2022-09-03 18:43:20 +0530

    Add files via upload

---
+++ greatest_of_2_cmd.java
@@ -0,0 +1,10 @@
+public class greatest_of_2_cmd {    
+    public static void main(String[] args) {    
+        int x =Integer.parseInt(args[0]);    
+        int y = Integer.parseInt(args[1]);    
+        if(x>y)
+        System.out.println("x is greater");    
+        else
+        System.out.println("y is greater");
+    }      
+}
