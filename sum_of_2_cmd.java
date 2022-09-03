7305a092279ccf43442d4c67d46443aca18693a6
santhosh <105176741+santhosh-programmer@users.noreply.github.com>
2022-09-03 18:39:54 +0530

    Add files via upload

---
+++ sum_of_2_cmd.java
@@ -0,0 +1,8 @@
+public class sum_of_2_cmd {
+    public static void main(String[] args) {
+        int a=Integer.parseInt(args[0]);
+        int b=Integer.parseInt(args[1]);
+        int sum=a+b;
+        System.out.println("sum: "+sum);
+    }
+}
