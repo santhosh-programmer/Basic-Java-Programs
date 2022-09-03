04bd5618b1436822319c4e858f15318f6843dae1
santhosh <105176741+santhosh-programmer@users.noreply.github.com>
2022-09-03 18:43:33 +0530

    Add files via upload

---
+++ for_loop.java
@@ -0,0 +1,10 @@
+class for_loop{ 
+    public static void main(String args[]) {        
+        for (int j = 0; j < 5; j++) {
+            if (j == 4) 
+                break;  
+            System.out.println(j); 
+        } 
+        System.out.println("After loop"); 
+    } 
+}
