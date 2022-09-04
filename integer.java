0b120d524381d9301f1bff45d75eb40eb89b614c
santhosh <105176741+santhosh-programmer@users.noreply.github.com>
2022-09-04 10:02:31 +0530

    Delete integer.java

--- integer.java
+++
@@ -1,7 +0,0 @@
-public class integer {
-    public static void main(String[] args) {
-        System.out.println("Integers: ");
-        for(String i:args)
-        System.out.println(Integer.parseInt(i));
-    }
-}
