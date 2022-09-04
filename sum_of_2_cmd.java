862756814b6544d408b195489b422a20a767774e
santhosh <105176741+santhosh-programmer@users.noreply.github.com>
2022-09-04 10:06:37 +0530

    Delete sum_of_2_cmd.java

--- sum_of_2_cmd.java
+++
@@ -1,8 +0,0 @@
-public class sum_of_2_cmd {
-    public static void main(String[] args) {
-        int a=Integer.parseInt(args[0]);
-        int b=Integer.parseInt(args[1]);
-        int sum=a+b;
-        System.out.println("sum: "+sum);
-    }
-}
